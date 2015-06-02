af8f80db9b8bb7349dc29865b4f62e76a62a5e2e
Rafael Ramirez <mrsblast@gmail.com>
2015-06-01 20:09:13 -0400

    [EDT] Added the image field in the model product.attribute.value using inheritance.

--- pos_kingdom.py
+++ pos_kingdom.py
@@ -1405,7 +1405,7 @@ class pos_category(osv.osv):
                  "resized as a 128x128px image, with aspect ratio preserved. "\
                  "Use this field in form views or some kanban views."),
         'image_small': fields.function(_get_image, fnct_inv=_set_image,
-            string="Smal-sized image", type="binary", multi="_get_image",
+            string="Small-sized image", type="binary", multi="_get_image",
             store={
                 'pos.category': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10),
             },
@@ -1413,6 +1413,44 @@ class pos_category(osv.osv):
                  "resized as a 64x64px image, with aspect ratio preserved. "\
                  "Use this field anywhere a small image is required."),
     }
+
+
+class product_attribute_value(osv.osv):
+    _inherit = 'product.attribute.value'
+
+    def _get_image(self, cr, uid, ids, name, args, context=None):
+        result = dict.fromkeys(ids, False)
+        for obj in self.browse(cr, uid, ids, context=context):
+            result[obj.id] = tools.image_get_resized_images(obj.image)
+        return result
+    
+    def _set_image(self, cr, uid, id, name, value, args, context=None):
+        return self.write(cr, uid, [id], {'image': tools.image_resize_image_big(value)}, context=context)
+
+    _columns = {
+        'image': fields.binary("Image",help="This field holds the image used as image for the attribute value, limited to 1024x1024px."),
+        'image_medium': fields.function(_get_image, fnct_inv=_set_image, string="Medium-sized image", type="binary", multi="_get_image",
+        store={
+            'product.attribute.value': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10)
+        },
+        help="Medium-sized image of the attribute value, It is automatically "\
+             "resized as a 128x128px image, with aspect ratio preserved. "\
+             "Use this field in form views or some kanban views."),
+
+        'image_small': fields.function(_get_image, fnct_inv=_set_image, string="Small-sized image", type="binary", multi="_get_image",
+        store={
+            'product.attribute.value': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10)
+        },
+        help="Small-sized image of the attribute value, It is automatically "\
+             "resized as a 64x64px image, with aspect ratio preserved. "\
+             "Use this field anywhere a small image is required."),
+
+
+    }
+
+
+
+
 
 class product_template(osv.osv):
     _inherit = 'product.template'
