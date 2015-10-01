53ef3013141757683315ace3e39a1a40bfcf812f
Rafael Ramirez <mrsblast@gmail.com>
2015-09-30 20:02:52 -0400

    [RFT] Refactor res_company extra fields.

--- res_company.py
+++ res_company.py
@@ -5,6 +5,6 @@ from openerp import models, fields, api
 
 class res_company(models.Model):
     _inherit = 'res.company'
-    branch_office = fields.Char()
+    branch_office = fields.Char('Branch Name',help='Branch Name')
     invoice_legend = fields.Text()
 
